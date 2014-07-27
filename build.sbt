64b68a23d94ef3aacefdb438d66d7762ab12851e
Sergey Berezhnoy <serg.berezhnoy@gmail.com>
2014-07-27 21:08:06 +0200

    Proof of functionality.

---
+++ build.sbt
@@ -0,0 +1,19 @@
+// set the name of the project
+name := "Coordinate Helper"
+
+version := "1.0"
+
+organization := "qen.helper"
+
+// set the Scala version used for the project
+scalaVersion := "2.11.0"
+
+EclipseKeys.withSource := true
+
+libraryDependencies ++= Seq( 
+"org.seleniumhq.selenium" % "selenium-java" % "2.42.2" % "compile",
+"org.scalatest" % "scalatest_2.11" % "2.2.0" % "test",
+"de.micromata.jak" % "JavaAPIforKml" % "2.2.0"
+)
+
+resolvers += "Java.net Maven 2 Repository" at "http://download.java.net/maven/2"
---
+++ src/main/scala/qen/helper/coordinate/HelperApplication.scala
@@ -0,0 +1,28 @@
+package qen.helper.coordinate
+import org.openqa.selenium._
+import org.openqa.selenium.htmlunit._
+import java.io.File
+import de.micromata.opengis.kml.v_2_2_0.Kml
+
+
+object HelperApplication extends App {
+
+//    val driver = new HtmlUnitDriver
+//    driver.get("http://quest.ua/Login.aspx?return=/Default.aspx");
+//    println(driver.getCurrentUrl());
+//    val loginField = driver.findElement(By.id("txtLogin"));
+//    loginField.sendKeys("COFEINE");
+//    val passwordField = driver.findElement(By.id("txtPassword"));
+//    passwordField.sendKeys("s7t9i5x1");
+//    val loginButton = driver.findElement(By.xpath("//input[@type='submit']"));
+//    loginButton.click();
+//    println(driver.getCurrentUrl());
+//    val enterGameButtom = driver.findElement(By.xpath("//a[@class='glassbutton ']"));
+//    enterGameButtom.click();
+//    println(driver.getCurrentUrl());
+    
+    val kml: Kml = new Kml();
+    kml.createAndSetPlacemark().withName("London, UK").withOpen(true).createAndSetPoint().addToCoordinates(-0.126236, 51.500152);
+    kml.marshal(new File("HelloKml.kml"));
+    
+}
